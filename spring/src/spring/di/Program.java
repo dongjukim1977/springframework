bc5f8fb26d9eb79f155ad65bd3e115b270b08d6b
dongju.kim <cratiation@gmail.com>
2022-04-12 10:02:29 +0900

    first commit

---
+++ spring/src/spring/di/Program.java
@@ -0,0 +1,36 @@
+package spring.di;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
+import spring.di.ui.ExamConsole;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		Exam exam = context.getBean(Exam.class);
+		System.out.println(exam.toString());
+		
+		ExamConsole console = context.getBean(ExamConsole.class);
+		console.print();
+		
+		
+		//List<Exam> exams = new ArrayList<>();
+		// setting.xml 에서 exams 객체 생성
+		//exams.add(new NewlecExam(1,1,1,1));
+		
+		//for(Exam e : exams) {
+		//	System.out.println(e);
+		//}
+
+	}
+
+}
